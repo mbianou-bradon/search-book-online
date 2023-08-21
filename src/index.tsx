8ade25c7e0eef583a17056b7255f61a6a1ada61b
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-08-21 06:48:18 +0100

    Installed graphql and apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,18 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import './index.css';
+import { ApolloProvider } from '@apollo/client';
+import client from './query/server/apolloServer';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
